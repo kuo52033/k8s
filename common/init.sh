01f483ca18fcaec9302148b65f34be4c35aea690
Tim Kuo <tim31422@gmail.com>
2024-04-06 17:05:10 +0800

    fix

--- common/init.sh
+++ common/init.sh
@@ -4,7 +4,7 @@ echo "[Task 1] Disable swap"
 sudo swapoff -a
 
 echo "[Task 2] install tools"
-sudo apt-get install -y nano jq net-tools >dev/null 2>&1
+sudo apt-get install -y nano jq net-tools >/dev/null 2>&1
 
 echo "[Task 3] Enable and Load Kernel modules"
 sudo tee /etc/modules-load.d/containerd.conf<<EOF
@@ -20,14 +20,14 @@ net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
 EOF
-sudo sysctl --system >dev/null 2>&1
+sudo sysctl --system >/dev/null 2>&1
 
 echo "[Task 5] Install container runtime"
-sudo apt install -y apt-transport-https ca-certificates curl gpg >dev/null 2>&1
+sudo apt install -y apt-transport-https ca-certificates curl gpg >/dev/null 2>&1
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >dev/null 2>&1
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null 2>&1
 sudo apt update >/dev/null 2>&1
 sudo apt install -y containerd.io > /dev/null 2>&1
 sudo containerd config default |sudo tee /etc/containerd/config.toml
@@ -38,7 +38,7 @@ sudo systemctl enable containerd >/dev/null 2>&1
 echo "[Task 6] : Install k8s dependencies"
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update >dev/null 2>&1
-sudo apt-get install -y kubelet kubeadm kubectl >dev/null 2>&1
-sudo apt-mark hold kubelet kubeadm kubectl >dev/null 2>&1
+sudo apt-get update >/dev/null 2>&1
+sudo apt-get install -y kubelet kubeadm kubectl >/dev/null 2>&1
+sudo apt-mark hold kubelet kubeadm kubectl >/dev/null 2>&1
 
