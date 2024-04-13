d77d81b15817d9831e6d9fce0f9f118f046ffaaf
Tim Kuo <tim31422@gmail.com>
2024-04-13 16:48:09 +0800

    add deploy shell script

---
+++ chat/deploy.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -xe
+
+# declare
+NAMESPACE="chat"
+WAIT_RESOURCE_CREATION_IN_SEC=300
+YAML_DIR_PATH="/chat"
+
+applyNamespace() {
+	echo "[INFO] apply $NAMESPACE namespace..."
+
+	kubectl apply -f $YAML_DIR_PATH/namespace.yaml
+
+	echo "[INFO] apply $NAMESPACE namespace done"
+}
+
+applyChatService() {
+	echo "[INFO] apply chat-deployment service ..."
+
+	kubectl apply -f $YAML_DIR_PATH/chat.deploy.yaml
+
+	timeout $WAIT_RESOURCE_CREATION_IN_SEC  kubectl rollout status deploy chat-deployment -w --namespace $NAMESPACE
+
+	echo "[INFO] apply chat-deployment service done"
+}
+
+applyNginxIngressController() {
+	echo "[INFO] apply nginx-ingress-controller..."
+
+	kubectl apply -f $YAML_DIR_PATH/nginx-ingress-controller.yaml
+	timeout $WAIT_RESOURCE_CREATION_IN_SEC kubectl rollout status deploy nginx-ingress-controller -w
+
+	echo "[INFO] apply nginx-ingress-controller done"
+}
+
+applyRedisService() {
+	echo "[INFO] apply redis service..."
+
+	kubectl apply -f $YAML_DIR_PATH/redis.deploy.yaml
+
+	timeout $WAIT_RESOURCE_CREATION_IN_SEC  kubectl rollout status deploy redis -w --namespace $NAMESPACE
+
+	echo "[INFO] apply redis service done"
+}
+
+# exit when any command failed
+set -e
+set -o pipefail
+
+# main
+applyNamespace
+applyNginxIngressController
+applyRedisService
+applyChatService
+
+exit 0
